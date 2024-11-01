aa2c140cebb422acecff71a2337a1eedcee41280
MrTAMIM404 <mdtamimchowdhory766@gmail.com>
2024-11-01 21:49:24 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -48,8 +48,8 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 
-    let gifUrl = 'https://i.imgur.com/b6INyOq.gif';
-let gifPath = __dirname + '/Nayan/join/join.gif';
+    let gifUrl = 'https://i.imgur.com/b6INyOq.mp4';
+let gifPath = __dirname + '/Nayan/join/join.mp4;
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
 .then(response => {
